4a271069b597050eee2487f1de70ecc99d94671b
hooshi <s.hoshyari@gmail.com>
2016-04-01 18:56:01 -0700

    The assembler framework is almost complete. Ready to curve an actual mesh.

---
+++ curving/src/linear_elasticity.C
@@ -0,0 +1,299 @@
+#include "linear_elasticity.hxx"
+
+
+/****************************************************************************
+ *                                Test Case                                 *
+ ****************************************************************************/
+
+/*
+ * MMS functions
+ */
+double TestCaseMMS::exact_solution(const Point& p, const uint n_unknown)
+{
+
+	double ans;
+
+	switch(n_unknown)
+	{
+	case U:
+		ans = cos(M_PI*p(X))*cos(M_PI*p(Y))*cosh(M_PI*p(Z));
+		break;
+	case V:
+		ans = sin(M_PI*p(X))*sin(2*M_PI*p(Y))*sinh(M_PI*p(Z));
+		break;
+	case W:
+		ans = sin(2*M_PI*p(X))*sin(M_PI*p(Y))*sinh(2*M_PI*p(Z));
+		break;
+	default:
+		libmesh_assert(0);
+		break;
+	}
+
+	return ans;
+}
+
+Gradient TestCaseMMS::exact_derivative(const Point& p, const uint n_unknown)
+{
+
+	Gradient g;
+	switch(n_unknown)
+	{
+	case U:
+		g(X) = -sin(M_PI * p(X)) * M_PI * cos(M_PI * p(Y)) * cosh(M_PI * p(Z));
+		g(Y) = -cos(M_PI * p(X)) * sin(M_PI * p(Y)) * M_PI * cosh(M_PI * p(Z));
+		g(Z) = cos(M_PI * p(X)) * cos(M_PI * p(Y)) * sinh(M_PI * p(Z)) * M_PI;
+		break;
+	case V:
+		g(X) = cos(M_PI * p(X)) * M_PI * sin(2. * M_PI * p(Y)) * sinh(M_PI * p(Z));
+		g(Y) = 2. * sin(M_PI * p(X)) * cos(2. * M_PI * p(Y)) * M_PI * sinh(M_PI * p(Z));
+		g(Z) = sin(M_PI * p(X)) * sin(2. * M_PI * p(Y)) * cosh(M_PI * p(Z)) * M_PI;
+		break;
+	case W:
+		g(X) = 2. * cos(2. * M_PI * p(X)) * M_PI * sin(M_PI * p(Y)) * sinh(2 * M_PI * p(Z));
+		g(Y) = sin(2. * M_PI * p(X)) * cos(M_PI * p(Y)) * M_PI * sinh(2 * M_PI * p(Z));
+		g(Z) = 2. * sin(2. * M_PI * p(X)) * sin(M_PI * p(Y)) * cosh(2. * M_PI * p(Z)) * M_PI;
+		break;
+	default:
+		libmesh_assert(0);
+	}
+
+	return g;
+}
+
+double TestCaseMMS::right_hand_side(const Point& p, const uint n_unknown)
+{
+	double f;
+	const double x=p(X), y=p(Y), z=p(Z);
+	const double pi=M_PI, mu=_material.mu, lambda=_material.lambda;
+
+	switch(n_unknown)
+	{
+	case U:
+		f = (double) (2 * mu + lambda) * cos(pi * x) * pi * pi * cos(pi * y) * cosh(pi * z) - 0.2e1 * (double) lambda * cos(pi * x) * pi * pi * cos(0.2e1 * pi * y) * sinh(pi * z) - 0.4e1 * (double) lambda * cos(0.2e1 * pi * x) * pi * pi * sin(pi * y) * cosh(0.2e1 * pi * z) - (double) mu * (-cos(pi * x) * cos(pi * y) * pi * pi * cosh(pi * z) + 0.2e1 * cos(pi * x) * pi * pi * cos(0.2e1 * pi * y) * sinh(pi * z)) - (double) mu * (cos(pi * x) * cos(pi * y) * pi * pi * cosh(pi * z) + 0.4e1 * cos(0.2e1 * pi * x) * pi * pi * sin(pi * y) * cosh(0.2e1 * pi * z));
+		break;
+	case V:
+		f = -mu * (sin(pi * x) * pi * pi * sin(pi * y) * cosh(pi * z) - sin(pi * x) * pi * pi * sin(0.2e1 * pi * y) * sinh(pi * z)) - lambda * sin(pi * x) * pi * pi * sin(pi * y) * cosh(pi * z) + 0.4e1 * (0.2e1 * mu + lambda) * sin(pi * x) * sin(0.2e1 * pi * y) * pi * pi * sinh(pi * z) - 0.2e1 * lambda * sin(0.2e1 * pi * x) * cos(pi * y) * pi * pi * cosh(0.2e1 * pi * z) - mu * (sin(pi * x) * pi * pi * sin(0.2e1 * pi * y) * sinh(pi * z) + 0.2e1 * sin(0.2e1 * pi * x) * cos(pi * y) * pi * pi * cosh(0.2e1 * pi * z));
+		break;
+	case W:
+		f = -mu * (-sin(pi * x) * pi * pi * cos(pi * y) * sinh(pi * z) - 0.4e1 * sin(0.2e1 * pi * x) * pi * pi * sin(pi * y) * sinh(0.2e1 * pi * z)) - mu * (0.2e1 * sin(pi * x) * cos(0.2e1 * pi * y) * pi * pi * cosh(pi * z) - sin(0.2e1 * pi * x) * pi * pi * sin(pi * y) * sinh(0.2e1 * pi * z)) + lambda * sin(pi * x) * pi * pi * cos(pi * y) * sinh(pi * z) - 0.2e1 * lambda * sin(pi * x) * cos(0.2e1 * pi * y) * pi * pi * cosh(pi * z) - 0.4e1 * (0.2e1 * mu + lambda) * sin(0.2e1 * pi * x) * sin(pi * y) * sinh(0.2e1 * pi * z) * pi * pi;
+		break;
+	default:
+		libmesh_assert(0);
+	}
+
+	return f;
+}
+
+void TestCaseMMS::boundary_penalty(const Point& p, const uint , const uint n_unknown, double &t, double &q)
+{
+	double penalty = 1.e10;
+	q = penalty;
+	t = penalty*exact_solution(p,n_unknown);
+}
+
+
+/****************************************************************************
+ *                             Assembler                                    *
+ ****************************************************************************/
+
+void LinearElasticity::evaluate_errors()
+{
+	ExactValue eval(_test_case);
+	ExactDeriv eder(_test_case);
+
+	ExactSolution esol(es);
+	esol.attach_exact_value(0, &eval);
+	esol.attach_exact_deriv(0, &eder);
+	esol.extra_quadrature_order(1);
+	esol.compute_error("Elasticity", "u");
+	esol.compute_error("Elasticity", "v");
+	esol.compute_error("Elasticity", "w");
+
+	std::cout << esol.l2_error("Elasticity", "u") << " " <<esol.l2_error("Elasticity", "v") << " " << esol.l2_error("Elasticity", "w") << std::endl;
+	std::cout << esol.h1_error("Elasticity", "u") << " " <<esol.h1_error("Elasticity", "v") << " " << esol.h1_error("Elasticity", "w") << std::endl;
+}
+
+
+/**
+ * Assemble the system matrix and right-hand side vector.
+ */
+void LinearElasticity::assemble()
+{
+	// mesh and dimensions
+	const MeshBase& mesh = es.get_mesh();
+	const uint dim = mesh.mesh_dimension();
+
+	// the system and var numbers
+	LinearImplicitSystem& system = es.get_system<LinearImplicitSystem>("Elasticity");
+	const uint u_var = system.variable_number ("u");
+
+	// Dof Map
+	const DofMap& dof_map = system.get_dof_map();
+
+	// FE for volume integrals
+	FEType fe_type = dof_map.variable_type(u_var);
+	UniquePtr<FEBase> fe (FEBase::build(dim, fe_type));
+	QGauss qrule (dim, fe_type.default_quadrature_order());
+	fe->attach_quadrature_rule (&qrule);
+
+	// shape functions and integration weights
+	const std::vector<Real>& JxW = fe->get_JxW();
+	const std::vector<std::vector<Real> >& phi = fe->get_phi();
+	const std::vector<std::vector<RealGradient> >& dphi = fe->get_dphi();
+
+	// FE for boundary integrals
+	UniquePtr<FEBase> fe_face (FEBase::build(dim, fe_type));
+	QGauss qface(dim-1, fe_type.default_quadrature_order());
+	fe_face->attach_quadrature_rule (&qface);
+
+	// Shape function and integration weigths
+	const std::vector<std::vector<Real> >&  phi_face = fe_face->get_phi();
+	const std::vector<Real>& JxW_face = fe_face->get_JxW();
+
+	// Location of quadrature points
+	const std::vector<Point>& q_point = fe->get_xyz();
+	const std::vector<Point >& qface_point = fe_face->get_xyz();
+
+	// Right hand side mats and vectors
+	DenseMatrix<Number> Ke;
+	DenseSubMatrix<Number> Ke_var[3][3] =
+	{
+			{DenseSubMatrix<Number>(Ke), DenseSubMatrix<Number>(Ke), DenseSubMatrix<Number>(Ke)},
+			{DenseSubMatrix<Number>(Ke), DenseSubMatrix<Number>(Ke), DenseSubMatrix<Number>(Ke)},
+			{DenseSubMatrix<Number>(Ke), DenseSubMatrix<Number>(Ke), DenseSubMatrix<Number>(Ke)}
+	};
+
+	DenseVector<Number> Fe;
+	DenseSubVector<Number> Fe_var[3] =
+	{DenseSubVector<Number>(Fe), DenseSubVector<Number>(Fe), DenseSubVector<Number>(Fe)};
+
+	// Dof indices
+	std::vector<dof_id_type> dof_indices;
+	std::vector< std::vector<dof_id_type> > dof_indices_var(3);
+
+	/*
+	 * For over all elements
+	 */
+	MeshBase::const_element_iterator       el     = mesh.active_local_elements_begin();
+	const MeshBase::const_element_iterator end_el = mesh.active_local_elements_end();
+
+	for ( ; el != end_el; ++el)
+	{
+		// pointer to element
+		const Elem* elem = *el;
+
+		// get dof indices
+		dof_map.dof_indices (elem, dof_indices);
+		for(unsigned int var=0; var<3; var++)
+		{
+			dof_map.dof_indices (elem, dof_indices_var[var], var);
+		}
+
+		const unsigned int n_dofs   = dof_indices.size();
+		const unsigned int n_var_dofs = dof_indices_var[0].size();
+
+		// reinit element
+		fe->reinit (elem);
+
+		// position the vector and matrices
+		Ke.resize (n_dofs,n_dofs);
+		for(unsigned int var_i=0; var_i<3; var_i++)
+			for(unsigned int var_j=0; var_j<3; var_j++)
+			{
+				Ke_var[var_i][var_j].reposition (var_i*n_var_dofs, var_j*n_var_dofs, n_var_dofs, n_var_dofs);
+			}
+
+		Fe.resize (n_dofs);
+		for(unsigned int var=0; var<3; var++)
+		{
+			Fe_var[var].reposition (var*n_var_dofs, n_var_dofs);
+		}
+
+		/*
+		 * Volume quadrature points
+		 */
+		 for (unsigned int qp=0; qp<qrule.n_points(); qp++)
+		 {
+
+			 // Assemble the Jacobian
+			 for (unsigned int dof_i=0; dof_i<n_var_dofs; dof_i++)
+				 for (unsigned int dof_j=0; dof_j<n_var_dofs; dof_j++)
+				 {
+					 for(unsigned int i=0; i<3; i++)
+						 for(unsigned int j=0; j<3; j++)
+							 for(unsigned int k=0; k<3; k++)
+								 for(unsigned int l=0; l<3; l++)
+								 {
+									 Ke_var[i][j](dof_i,dof_j) += JxW[qp] *
+											 elasticity_tensor(i,j,k,l) *
+											 dphi[dof_i][qp](k)* dphi[dof_j][qp](l);
+								 }
+				 }
+
+			 // Assemble the right hand side
+			 DenseVector<Number> f_vec(3);
+			 f_vec(0) = _test_case.right_hand_side(q_point[qp],0);
+			 f_vec(1) = _test_case.right_hand_side(q_point[qp],1);
+			 f_vec(2) = _test_case.right_hand_side(q_point[qp],2);
+			 for (unsigned int dof_i=0; dof_i<n_var_dofs; dof_i++)
+			 {
+				 for(unsigned int i=0; i<3; i++)
+				 {
+					 Fe_var[i](dof_i) += JxW[qp] *
+							 ( f_vec(i) * phi[dof_i][qp] );
+				 }
+			 }
+		 } // End of volume quadrature points
+
+
+		 /*
+		  * Boundary quadrature points for the penalty method
+		  */
+		 DenseVector<Number> t_vec(3), q_vec(3);
+
+		 // find a boundary side
+		 for (unsigned int side=0; side<elem->n_sides(); side++)
+			 if (elem->neighbor(side) == NULL)
+			 {
+				 fe_face->reinit(elem, side);
+
+				 for (unsigned int qp=0; qp<qface.n_points(); qp++)
+				 {
+					 // Find the boundary id
+					 const uint id = mesh.get_boundary_info().boundary_id(elem,side);
+
+					 // Get the penalty parameters
+					 for (uint i=0; i<3 ; i++)
+					 {
+						 _test_case.boundary_penalty(qface_point[qp],id,i,t_vec(i),q_vec(i));
+					 }
+
+					 // Add contribution to RHS
+					 for (unsigned int dof_i=0; dof_i<n_var_dofs; dof_i++)
+					 {
+						 for(unsigned int i=0; i<3; i++)
+						 {
+
+							 Fe_var[i](dof_i) += JxW_face[qp] *
+									 ( t_vec(i) * phi_face[dof_i][qp] );
+						 }
+					 }
+
+					 // Add contribution to LHS
+					 for (unsigned int dof_i=0; dof_i<n_var_dofs; dof_i++)
+						 for (unsigned int dof_j=0; dof_j<n_var_dofs; dof_j++)
+						 {
+							 for(unsigned int i=0; i<3; i++)
+							 {
+								 Ke_var[i][i](dof_i,dof_j) += JxW_face[qp] *
+										 q_vec(i) * phi_face[dof_j][qp]*phi_face[dof_i][qp];
+							 }
+						 }
+
+				 } // end of quadrature points
+			 } // end of boundary side
+
+		 system.matrix->add_matrix (Ke, dof_indices);
+		 system.rhs->add_vector    (Fe, dof_indices);
+	} // end of elements
+}
---
+++ curving/src/linear_elasticity.hxx
@@ -0,0 +1,286 @@
+
+
+#ifndef TEST_CASE_HXX
+#define TEST_CASE_HXX
+
+/****************************************************************************
+ *                                 Includes                                 *
+ ****************************************************************************/
+// C++ include files that we need
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+
+// libMesh includes
+#include "libmesh/libmesh_config.h"
+#include "libmesh/libmesh.h"
+#include "libmesh/mesh.h"
+#include "libmesh/mesh_generation.h"
+#include "libmesh/exodusII_io.h"
+#include "libmesh/gnuplot_io.h"
+#include "libmesh/linear_implicit_system.h"
+#include "libmesh/equation_systems.h"
+#include "libmesh/fe.h"
+#include "libmesh/quadrature_gauss.h"
+#include "libmesh/dof_map.h"
+#include "libmesh/sparse_matrix.h"
+#include "libmesh/numeric_vector.h"
+#include "libmesh/dense_matrix.h"
+#include "libmesh/dense_submatrix.h"
+#include "libmesh/dense_vector.h"
+#include "libmesh/dense_subvector.h"
+#include "libmesh/perf_log.h"
+#include "libmesh/elem.h"
+#include "libmesh/boundary_info.h"
+#include "libmesh/zero_function.h"
+#include "libmesh/dirichlet_boundaries.h"
+#include "libmesh/string_to_enum.h"
+#include "libmesh/getpot.h"
+#include "libmesh/solver_configuration.h"
+#include "libmesh/petsc_linear_solver.h"
+#include "libmesh/petsc_macro.h"
+#include "libmesh/exact_solution.h"
+
+// Bring in everything from the libMesh namespace
+using namespace libMesh;
+
+
+/****************************************************************************
+ *                                    Enum                                  *
+ ****************************************************************************/
+enum Variables{U=0, V, W};
+enum Coordinates{X=0, Y, Z};
+
+
+/****************************************************************************
+ *                                 Material                                 *
+ ****************************************************************************/
+
+class Material
+{
+	Material(){}
+public:
+	double lambda, mu;
+
+	Material(const double lambda_val, const double mu_val):
+		lambda(lambda_val),
+		mu(mu_val)
+	{}
+};
+
+/****************************************************************************
+ *                                Test Case                                *
+ ****************************************************************************/
+
+/**
+ * A class containing data for each test case (rhs, bdry condition).
+ */
+class TestCase
+{
+
+protected:
+	  Material &_material;
+	  bool _has_exact_solution;
+	  bool _has_nonzero_rhs;
+
+public:
+
+	  // constructor
+	  TestCase(Material &material, bool has_exact_solution, bool has_nonzero_rhs):
+		  _material(material),
+		  _has_exact_solution(has_exact_solution),
+		  _has_nonzero_rhs(has_nonzero_rhs)
+		{}
+
+	  // destructor
+	  virtual ~TestCase(){}
+
+	  /**
+	    * Exact solution if available.
+	    */
+	  virtual double exact_solution(const Point& , const uint )
+	  { libmesh_assert(0); return 0;}
+
+	  /**
+	    * Exact derivative if available.
+	    */
+	  virtual Gradient exact_derivative(const Point& , const uint )
+	  {libmesh_assert(0); Gradient g; return g;}
+
+	  /**
+	   * Right hand side of the equations.
+	   */
+	  virtual double right_hand_side(const Point& , const uint )
+	  {libmesh_assert(0); return 0;}
+
+	  /**
+	   * Dirichlet boundary conditions parameters.
+	   * Must always be overriden, so pure virtual.
+	   */
+	  virtual void boundary_penalty(const Point& p, const uint id, const uint n_unknown, double &t, double &q) = 0;
+
+	  /**
+	   * Query functions
+	   */
+	  bool has_exact_solution()
+	  {return _has_exact_solution;}
+
+	  bool has_nonzero_rhs()
+	  {return _has_nonzero_rhs;}
+};
+
+/*
+ * Manufactured solution test case.
+ */
+class TestCaseMMS: public TestCase
+{
+public:
+
+	  // constructor
+	  TestCaseMMS(Material &material):
+		  TestCase(material, true, true)
+		{}
+
+	  // destructor
+	  ~TestCaseMMS(){}
+
+	  double exact_solution(const Point& p, const uint n_unknown);
+	  Gradient exact_derivative(const Point& p, const uint n_unknown);
+	  double right_hand_side(const Point& p, const uint n_unknown);
+	  void boundary_penalty(const Point& p, const uint id, const uint n_unknown, double &t, double &q);
+};
+
+
+/****************************************************************************
+ *                    Exact solution function wrappers                      *
+ ****************************************************************************/
+
+/**
+ * Finding the errors
+ */
+class ExactValue: public FunctionBase<Number>
+{
+private:
+	TestCase *_test_case;
+
+public:
+
+	// constructor and destructor
+	ExactValue(TestCase &test_case):
+		_test_case(&test_case)
+	{
+		libmesh_assert(_test_case->has_exact_solution());
+		this->_initialized=true;
+	}
+	~ExactValue(){}
+
+
+	// libmesh musts
+	UniquePtr<FunctionBase<Number> > clone() const libmesh_override
+	{
+		return UniquePtr<FunctionBase<Number> >  (new ExactValue(*_test_case));
+	}
+
+	Number operator() (const Point & p, const Real = 0) libmesh_override
+			{
+		return _test_case->exact_solution(p,0);
+			}
+
+	virtual void operator() (const Point & p, const Real, DenseVector<Number> & output) libmesh_override
+			{
+		unsigned int size = output.size();
+		for (unsigned int i=0; i != size; ++i)
+			output(i) = _test_case->exact_solution(p,i);
+			}
+};
+
+class ExactDeriv: public FunctionBase<Gradient>
+{
+private:
+	TestCase *_test_case;
+
+	// not allowed
+	ExactDeriv(){}
+
+public:
+
+	// constructor and destructor
+	ExactDeriv(TestCase &test_case):
+		_test_case(&test_case)
+	{
+		libmesh_assert(_test_case->has_exact_solution());
+		this->_initialized=true;
+	}
+	~ExactDeriv(){}
+
+
+	// libmesh musts
+	UniquePtr<FunctionBase<Gradient> > clone() const libmesh_override
+	{
+		return UniquePtr<FunctionBase<Gradient> >  (new ExactDeriv(*_test_case));
+	}
+
+	Gradient operator() (const Point & p, const Real = 0) libmesh_override
+			{
+		return _test_case->exact_derivative(p,0);
+			}
+
+	virtual void operator() (const Point & p, const Real, DenseVector<Gradient> & output) libmesh_override
+			{
+		unsigned int size = output.size();
+		for (unsigned int i=0; i != size; ++i)
+			output(i) = _test_case->exact_derivative(p,i);
+			}
+};
+
+/****************************************************************************
+ *                             Assembler                                    *
+ ****************************************************************************/
+
+class LinearElasticity : public System::Assembly
+{
+private:
+  EquationSystems &es;
+  Material &_material;
+  TestCase &_test_case;
+
+public:
+
+  LinearElasticity (EquationSystems &es_in, Material &material, TestCase &tc) :
+    es(es_in),
+    _material(material),
+    _test_case(tc)
+  { }
+
+  /**
+   * Kronecker delta function.
+   */
+  Real kronecker_delta(const uint i, const uint j)
+  {
+    return i == j ? 1. : 0.;
+  }
+
+  /**
+   * Evaluate the fourth order tensor (C_ijkl) that relates stress to strain.
+   */
+  Real elasticity_tensor(const uint i, const uint j, const uint k, const uint l)
+  {
+
+    return _material.lambda * kronecker_delta(i,k) * kronecker_delta(j,l) +
+           _material.mu * (kronecker_delta(i,j) * kronecker_delta(k,l) + kronecker_delta(i,l) * kronecker_delta(j,k));
+  }
+
+  /**
+   * Find the errors if we have a manufactured solution.
+   */
+  void evaluate_errors();
+
+
+  /**
+   * Assemble the system matrix and right-hand side vector.
+   */
+  void assemble();
+
+ };
+
+#endif /* TEST_CASE_HXX */
